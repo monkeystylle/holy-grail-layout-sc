521aef0010227fa946e7caf73c45171fa33889b2
meng <monkeystylle@gmail.com>
2022-05-31 20:36:40 +0800

    holy grail layout Styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,48 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Title>My page ATUA</Title>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-        neque, ab eligendi natus nostrum optio.
-      </p>
-    </>
+    <PageWrapper>
+      <Header></Header>
+      <Nav></Nav>
+      <Main></Main>
+      <Aside></Aside>
+      <Footer></Footer>
+    </PageWrapper>
   );
 };
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const PageWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 200px 1fr 150px;
+  grid-template-rows: 4rem 1fr 5rem;
+  grid-template-areas:
+    'header header header'
+    'sidebar main ad'
+    'footer footer footer';
+  min-height: 100%;
+`;
+
+const Header = styled.header`
+  grid-area: header;
+  background-color: lightyellow;
+`;
+const Nav = styled.nav`
+  grid-area: sidebar;
+  background-color: mediumaquamarine;
+`;
+
+const Main = styled.main`
+  grid-area: main;
+  background-color: lightblue;
+`;
+
+const Aside = styled.aside`
+  grid-area: ad;
+  background-color: lavender;
+`;
+const Footer = styled.footer`
+  grid-area: footer;
+  background-color: papayawhip;
 `;
 
 export default Home;
